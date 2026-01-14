6a826f4917e506416542ef435d9bfac7e97cfae2
Mukunda-Rijyal <mukundarijyal@gmail.com>
2026-01-14 17:50:31 +0545

    Utils code

--- src/index.js
+++ src/index.js
@@ -7,7 +7,15 @@ dotenv.config({
 })
 
 connectDB()
-
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running on port ${process.env.PORT || 8000}`)
+        })
+    })
+    .catch((error) => {
+        console.error("Failed to connect to the database:", error)
+        process.exit(1)
+    })
 
 /*
 
